7f1f3a4ae0d8c396058ef726c6107e3b6c97e334
Caroline Ignácio <carolineignacior@gmail.com>
2020-03-31 21:28:40 -0300

    Configure request to API

--- src/App.js
+++ src/App.js
@@ -1,8 +1,34 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 import './index.css';
 
 function App() {
+
+  const axios = require('axios');
+
+  const [temp, setTemp] = useState([]);
+
+  const lat = -28.6775;
+  const lon = -49.36972;
+
+  const params = {
+    access_key: '1ff57548e829e7d039e72e93bd303898',
+    query: `${lat},${lon}`
+  }
+
+  useEffect(() => {
+
+    axios.get('http://api.weatherstack.com/current', {params})
+    .then(response => {
+      console.log(response.data)
+      setTemp(response.data.current.temperature);
+    }).catch(error => {
+      console.log(error);
+    });
+
+  }, []);
+
+
   return (
     <div>
       <div className="container">
@@ -20,8 +46,8 @@ function App() {
           </form>
         </div>
         <div className="return-content">
-          <h1>Result</h1> 
-          <p>Value</p>
+          <h1>Result</h1>
+          <p>{temp}</p>
         </div>
       </div>
       <footer>Caroline, 2020.</footer>
